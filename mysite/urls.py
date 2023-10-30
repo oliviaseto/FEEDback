eb2d3b27674329fa1f09b64fb19d6b4671bb7b47
Olivia Seto <oliviaseto5@gmail.com>
2023-10-30 18:38:49 -0400

    changed foodapp to mysite in wsgi file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for foodapp project.
+URL configuration for feedback project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
